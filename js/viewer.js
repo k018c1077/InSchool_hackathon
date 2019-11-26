a6c0308763b2768b86926f0ac92a719149be97f1
k019c1077 <yukito.azuma16@gmail.com>
2019-11-26 14:03:42 +0900

    Update viewer.js

--- js/viewer.js
+++ js/viewer.js
@@ -1,7 +1,7 @@
-$(function () {
+window.onload = function () {
     let parsed_data = "";
     $.get("./markdown/Pre-1/Pre-1.md", function (e) { 
         parsed_data = marked(e);
         $("#markdown").html(parsed_data);
     })
-}) 
+}
