bb9046f799bc26ac0b0eb23deaea20d304608ae2
k019c1077 <yukito.azuma16@gmail.com>
2019-11-26 14:02:42 +0900

    修正

--- js/viewer.js
+++ js/viewer.js
@@ -2,6 +2,6 @@ $(function () {
     let parsed_data = "";
     $.get("./markdown/Pre-1/Pre-1.md", function (e) { 
         parsed_data = marked(e);
+        $("#markdown").html(parsed_data);
     })
-    $("#markdown").html(parsed_data);
 }) 
